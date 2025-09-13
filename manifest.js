0c54eb8a9e4de78b5453ae5b9c1421e14368e3c0
Cristiano10cr <167020718+Cristiano10cr@users.noreply.github.com>
2025-09-12 20:20:39 -0500

    manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,21 @@
+{
+  "name": "Catch the Fish",
+  "short_name": "PezGame",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#0077b6",
+  "theme_color": "#0077b6",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
 
